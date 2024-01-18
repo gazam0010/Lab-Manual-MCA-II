887acc77eaea7a1fd4be828deb1b2b019e56c29c
Suhel Khan <112371384+SuhelKhanCA@users.noreply.github.com>
2024-01-18 14:07:09 +0000

    fourth problem

---
+++ Week1/Q4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Q4{
+    public static void main(String[] args){
+        System.out.print("Enter temprature in Fahrenheit:");
+
+        Scanner sc = new Scanner(System.in);
+
+        double ft = sc.nextDouble();
+
+        double c = (ft - 32) / 1.8;
+        
+        System.out.println("Sum of two numbers:"+ c);
+    }
+}
