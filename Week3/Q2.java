8394a6d0bb92043d8788b68de9fe74b285bc7702
suhelkhanca <suhel.hata@gmail.com>
2024-01-21 22:05:36 +0530

    Printing series of squares and cubes upto 5

---
+++ Week3/Q2.java
@@ -0,0 +1,16 @@
+package Week3;
+public class Q2 {
+    public static void main(String[] args) {
+    
+    System.out.print("Square from 1 to 5 are : ");
+    for (int i = 1 ; i <= 5; i++) {
+        System.out.print((i*i) + " ");
+    }
+    System.out.println();
+
+    System.out.print("Cube from 1 to 5 are : ");
+    for (int i = 1; i <= 5; i++) {
+        System.out.print((i*i*i) + " ");
+    }       
+    }
+}
