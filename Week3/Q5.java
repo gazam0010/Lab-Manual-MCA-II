06556d5a7bb9121d37d2f5542a8597758f35c916
suhelkhanca <suhel.hata@gmail.com>
2024-01-21 22:49:02 +0530

    Floyd Triagle with slight modification

--- Week3/Q5.java
+++ Week3/Q5.java
@@ -2,6 +2,8 @@ package Week3;
 
 public class Q5 {
     public static void main(String[] args) {
+        // Originallay
+        System.out.print("Firstly");
         int a = 1;
         for (int i = 0; i <= 6; i++) {
            for (int j = 0; j < i; j++) {
@@ -9,6 +11,19 @@ public class Q5 {
             a++;
            }
            System.out.println();
+        }
+        // variation
+        System.out.println("\nAfter Variation");
+        int n = 6; // number of rows
+        int num = 1; // start with 1
+
+        for (int i = 1; i <= n; i++) {
+            num = i % 2; // alternate starting number for each row
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num = 1 - num; // flip between 1 and 0
+            }
+            System.out.println();
         }
     }
 }
