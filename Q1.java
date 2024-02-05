7715cf69c249a11090e217ed6d6f8dbacbb84942
Suhel Khan <112371384+SuhelKhanCA@users.noreply.github.com>
2024-02-05 07:09:07 +0000

    Week 1 first question

---
+++ Q1.java
@@ -0,0 +1,19 @@
+package Week5;
+class OddAndEven{
+	public int countOfOdd;
+    public int countOfOdd;
+
+    public void addNumber(int n){
+        
+    }
+}
+class TestOddAndEven{
+
+}
+
+// main class
+public class Week5{
+    public static void main(String[] args){
+
+    }
+}
